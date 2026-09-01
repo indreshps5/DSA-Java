c4b0ec473c73975a954eb3e225447a4292b65d8e
Indresh Pratap Singh <indreshpratapsinghh@gmail.com>
2026-09-01 16:29:39 +0530

    Added LeetCode 217 contains duplicate

---
+++ LeetCode/Arrays/217_ContainsDuplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set= new HashSet<>();
+        set.add(nums[0]);
+        for(int i=1; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+
+        return false;
+        
+    }
+}
